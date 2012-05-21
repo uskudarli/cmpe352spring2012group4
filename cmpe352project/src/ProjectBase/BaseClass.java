9ade6a091cebe47ddb3b36b13a7931547a88b1c0
semihaklar <semihaklar@gmail.com>
2012-05-21 18:47:33 +0000

    I changed the return value of testEmail(corrected)

--- cmpe352project/src/ProjectBase/BaseClass.java
+++ cmpe352project/src/ProjectBase/BaseClass.java
@@ -18,7 +18,7 @@ public class BaseClass {
 	public static boolean testPassword(String input, String password)
 	{
 		boolean retval = input.equals(password);
-		return retval;
+		return false;
   }
 
   public static boolean CheckNumberOfFollowers(int numberOfFollowers)
