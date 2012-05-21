edfc06b0c9200aca623b08553b2ead361a32fb17
basakcaprak@gmail.com <basakcaprak@gmail.com@ee13d6cd-83bf-24ef-2ad5-3c906ca3cf69>
2012-05-21 16:54:49 +0000

    I added "CheckNumberOfFollowers" and "main" classes.

--- trunk/cmpe352project/src/ProjectBase/BaseClass.java
+++ trunk/cmpe352project/src/ProjectBase/BaseClass.java
@@ -1,16 +1,26 @@
 package ProjectBase;
 
 public class BaseClass {
-	
+
+  public static void main (String args[])
+  {
+     System.out.println("Hello World from Basak");
+  }
+
 	public static boolean testEmail(String email)
 	{
-		boolean retval = email.contains("@");
-		return false;
-	}
+	 	boolean retval = email.contains("@");
+		 return false;
+  }
+
 	public static boolean testPassword(String input, String password)
 	{
 		boolean retval = input.equals(password);
 		return false;
-	}
+  }
 
+  public static boolean CheckNumberOfFollowers(int numberOfFollowers)
+  {
+    return (2000>numberOfFollowers);
+  }  
 }
