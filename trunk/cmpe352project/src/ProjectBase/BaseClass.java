81850f4ff48cf3a7f692e5e915c4a40bbe7bebba
basakcaprak@gmail.com <basakcaprak@gmail.com@ee13d6cd-83bf-24ef-2ad5-3c906ca3cf69>
2012-05-21 17:05:49 +0000

    I checked the code out and added "CheckNumberOfFollowers" and "main" classes. I commit the project back.

--- trunk/cmpe352project/src/ProjectBase/BaseClass.java
+++ trunk/cmpe352project/src/ProjectBase/BaseClass.java
@@ -5,6 +5,7 @@ public class BaseClass {
   public static void main (String args[])
   {
      System.out.println("Hello World from Basak");
+//each person should modify this line with his/her name
   }
 
 	public static boolean testEmail(String email)
