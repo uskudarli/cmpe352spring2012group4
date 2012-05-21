9efe7670aacdbe116d050dfcd42ef178aa26d2ea
semihaklar <semihaklar@gmail.com>
2012-05-21 18:43:51 +0000

    I changed the return value of testEmail

--- cmpe352project/src/ProjectBase/BaseClass.java
+++ cmpe352project/src/ProjectBase/BaseClass.java
@@ -5,19 +5,20 @@ public class BaseClass {
   public static void main (String args[])
   {
      System.out.println("Hello World from Gul");
+     System.out.println("Hello World from Semih");
 //each person should modify this line with his/her name
   }
 
 	public static boolean testEmail(String email)
 	{
 	 	boolean retval = email.contains("@");
-		 return false;
+		 return retval;
   }
 
 	public static boolean testPassword(String input, String password)
 	{
 		boolean retval = input.equals(password);
-		return false;
+		return retval;
   }
 
   public static boolean CheckNumberOfFollowers(int numberOfFollowers)
